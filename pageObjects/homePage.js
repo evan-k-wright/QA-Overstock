df57487400dc9d7ab69b804b6384e69afcd0dc33
Evan <rideburtonsnow3@gmail.com>
2018-04-18 13:48:24 -0600

    made page object

---
+++ pageObjects/homePage.js
@@ -0,0 +1,10 @@
+module.exports = {
+    url: 'https://www.overstock.com/',
+    elements: {
+        accountLogo: 'svg[class="icon-defaults"]',
+        overStockLogo: 'svg[class="overstock-logo"]',
+        overStockLogoText: 'svg[class="overstock-logo-text"]',
+        searchBar: 'fieldset[class="search-area"]',
+        searchSubmit: 'fieldset[class="submit"]',
+    },
+}
--- tests/uiTest.js
+++ tests/uiTest.js
@@ -6,5 +6,14 @@ module.exports = {
     after: browser => {
         browser
     },
-    
+    'Home Page UI Test': browser => {
+        let homePage = browser.page.homePage()
+        homePage.navigate()
+        homePage.waitForElementVisible('@overStockLogo', 5000)
+                .waitForElementVisible('@overStockLogoText', 5000)
+                .waitForElementVisible('@searchBar', 5000)
+                .waitForElementVisible('@searchSubmit', 5000)
+                .waitForElementVisible('@accountLogo', 5000)
+    }
+
 }
